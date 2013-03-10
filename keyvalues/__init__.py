de02b5b7deb62b96aefd4846704edfe1f8e151fd
Júlio César <julio@julioc.me>
2013-03-10 04:48:47 -0300

    Changed dir structure to use PyPi

---
+++ keyvalues/__init__.py
@@ -0,0 +1,4 @@
+__version_info__ = ('0', '1', '0')
+__version__ = '.'.join(__version_info__)
+
+from keyvalues.keyvalues import KeyValues
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name = "KeyValues",
+    version = __version__,
+    author = "Júlio César",
+    author_email = "julio@julioc.me",
+    description = ("Python implementation of Valve Key Values format."),
+    keywords = "valve keyvalues",
+    url = "https://github.com/JulioC/keyvalues-python",
+    packages=['keyvalues'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
+    long_description="""\
+    Module for parsing, using and writing files with Key Values format used by Valve.
+
+    The parser is supposed to follow the format specification, which is reproduced
+    on this Valve Developer Wiki page. The exception is for macros (eg #base, #include).
+
+    The KeyValues class has an interface compatible with Python dictionaries, with
+    addition to the load(filename) and save(filename) methods.
+    """,
+)
